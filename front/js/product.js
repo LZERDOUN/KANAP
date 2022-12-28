3ffeef0fc333426864de2f2f523152dbe08c0814
LZERDOUN <laura.gaguech@gmail.com>
2022-12-28 21:26:28 +0100

    Ajout quantité produit

--- front/js/product.js
+++ front/js/product.js
@@ -33,18 +33,13 @@ fetch("http://localhost:3000/api/products/" + productId)
 //-------------------------------------------------------------------------
 
 function displayProduct(product) {
-  //affichage nom de la page
   titlePage[0].innerHTML = product.name;
-  //affichage du titre produit
   productTitle.innerText = product.name;
-  //affichage de l'image produit
   productImage.insertAdjacentHTML(
     "afterbegin",
     `<img src=${product.imageUrl} alt=${product.altTxt}>`
   );
-  //affichage du prix du produit
   productPrice.innerText = product.price;
-  //affichage de la description du produit
   productDescription.innerText = product.description;
   //affichage du choix des couleur du produit
   for (let myColor of product.colors) {
@@ -54,3 +49,14 @@ function displayProduct(product) {
     listOfColor.appendChild(optionColor);
   }
 }
+
+//-------------------------------------------------------------------------
+//Gérer l'ajout de quantité de produit
+//-------------------------------------------------------------------------
+
+let listeQuantite = document.getElementById("quantity");
+let quantiteProduit;
+listeQuantite.addEventListener("change", function (ajouteUnArticle) {
+  quantiteProduit = ajouteUnArticle.target.value;
+  productId.quantiteChoisie = quantiteProduit;
+});
